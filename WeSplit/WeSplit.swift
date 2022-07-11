34c52708dedd5b135b0d5c7497c6df894ba07e03
Paul Crow <paucrow@synamedia.com>
2022-07-11 13:22:09 +0100

    Pickers in a Form

---
+++ WeSplit/WeSplit.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit.swift
+//  WeSplit
+//
+//  Created by paucrow on 08/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
