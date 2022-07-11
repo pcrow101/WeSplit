537d0813242df4a1f5659ad8df504cf52f4b4ff8
Paul Crow <paucrow@synamedia.com>
2022-07-11 11:04:45 +0100

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by paucrow on 08/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var tapCount = 0
+    
+    var body: some View {
+        Button("Tap Count \(tapCount)") {
+            self.tapCount += 1
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
